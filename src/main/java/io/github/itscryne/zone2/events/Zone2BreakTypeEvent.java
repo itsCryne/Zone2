1e245581e2bd7b67f2d85626917474e669325235
Patrick Longer <p.longer@gmx.de>
2020-01-27 20:24:19 +0100

    Initial commit

---
+++ src/main/java/io/github/itscryne/zone2/events/Zone2BreakTypeEvent.java
@@ -0,0 +1,28 @@
+package io.github.itscryne.zone2.events;
+
+import java.io.IOException;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+import io.github.itscryne.zone2.Zone2;
+import io.github.itscryne.zone2.perms.PermissionType;
+
+public class Zone2BreakTypeEvent implements Listener {
+    private Zone2 plugin;
+    public Zone2BreakTypeEvent(Zone2 plugin){
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onBlockBreak (BlockBreakEvent event) throws IOException {
+        Location eventLocation = event.getBlock().getLocation();
+        Player eventPlayer = event.getPlayer();
+
+        boolean allowed = Zone2PermCheck.isAllowed(eventLocation, eventPlayer, PermissionType.BREAK, this.plugin);
+        this.plugin.getLogger().info(String.valueOf(allowed));
+    }
+}
