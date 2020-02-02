ad4ae8c257f5725e5bed61e9c29f24e766517468
Patrick Longer <p.longer@gmx.de>
2020-02-02 19:46:50 +0100

    Initial commit

---
+++ src/main/java/io/github/itscryne/zone2/extensions/Zonecation.java
@@ -0,0 +1,67 @@
+package io.github.itscryne.zone2.extensions;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+
+import io.github.itscryne.zone2.Zone2;
+import io.github.itscryne.zone2.config.ConfigReader;
+import io.github.itscryne.zone2.spaces.PlayerZone;
+import io.github.itscryne.zone2.spaces.ServerZone;
+
+public class Zonecation extends Location {
+
+    public Zonecation(World world, double x, double y, double z) {
+        super(world, x, y, z);
+    }
+    public Zonecation(World world, double x, double y, double z, float yaw, float pitch){
+        super(world, x, y, z, yaw, pitch);
+    }
+
+    public boolean inPlayerZone() throws FileNotFoundException, IOException {
+        for (PlayerZone i : ConfigReader.getInstance(Zone2.getPlugin()).getPlayerZoneList()){
+            if (i.contains(this)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean inServerZone() throws FileNotFoundException, IOException {
+        for (ServerZone i : ConfigReader.getInstance(Zone2.getPlugin()).getServerZoneList()){
+            if (i.contains(this)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean inZone() throws FileNotFoundException, IOException {
+        return this.inPlayerZone() || this.inServerZone();
+    }
+
+    public List<ServerZone> getServerZones() throws FileNotFoundException, IOException {
+        List<ServerZone> serverZoneList = new ArrayList<>();
+        for(ServerZone i : ConfigReader.getInstance(Zone2.getPlugin()).getServerZoneList()){ //TODO: Remove Plugin
+            if (i.contains(this)){
+                serverZoneList.add(i);
+            }
+        }
+        return serverZoneList;
+    }
+
+    public List<PlayerZone> getPlayerZones() throws FileNotFoundException, IOException {
+        List<PlayerZone> playerZoneList = new ArrayList<>();
+        for(PlayerZone i : ConfigReader.getInstance(Zone2.getPlugin()).getPlayerZoneList()){ //TODO: Remove Plugin
+            if (i.contains(this)){
+                playerZoneList.add(i);
+            }
+        }
+        return playerZoneList;
+    }
+
+}
---
+++ src/main/java/io/github/itscryne/zone2/extensions/Zoneler.java
@@ -0,0 +1,65 @@
+package io.github.itscryne.zone2.extensions;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.bukkit.craftbukkit.v1_14_R1.CraftServer;
+import org.bukkit.craftbukkit.v1_14_R1.entity.CraftPlayer;
+
+import io.github.itscryne.zone2.perms.Permission;
+import io.github.itscryne.zone2.perms.PermissionType;
+import io.github.itscryne.zone2.spaces.PlayerZone;
+import io.github.itscryne.zone2.spaces.ServerZone;
+import io.github.itscryne.zone2.spaces.Zone;
+import net.minecraft.server.v1_14_R1.EntityPlayer;
+
+public class Zoneler extends CraftPlayer {
+
+    public Zoneler(CraftServer server, EntityPlayer entity) {
+        super(server, entity);
+    }
+
+        /**
+     *
+     * @param l Zonecation to check
+     * @param t PermissionType to check
+     * @return wether the player is allowed to do the action
+     * @throws IOException if it cant access a file et al
+     */
+    public boolean isAllowed(Zonecation l, PermissionType t) throws IOException {
+        boolean inPlayerZone = l.inPlayerZone();
+        boolean inServerZone = l.inServerZone();
+        boolean inZone = l.inZone();
+
+        List<PlayerZone> playerZoneList = l.getPlayerZones();
+        List<ServerZone> serverZoneList = l.getServerZones();
+
+        if (!inZone){
+            return this.hasPermission("Zone2.modifyNoZone");
+        }
+        if (inPlayerZone && inServerZone){
+            Zone highestPriority = Zone.getHighestPriorityZone(playerZoneList, serverZoneList);
+            if (highestPriority instanceof PlayerZone){
+                PlayerZone highestPriorityPlayerZone = (PlayerZone) highestPriority;
+                Permission neededPerm = new Permission(this, t);
+                Permission administratePerm = new Permission(this, PermissionType.MANAGE);
+                return highestPriorityPlayerZone.getPerms().contains(neededPerm)
+                        || highestPriorityPlayerZone.getPerms().contains(administratePerm)
+                        || highestPriorityPlayerZone.getPlayerUuid().equals(this.getUniqueId())
+                        || this.hasPermission("Zone2.modifyPlayerZone");
+            } else {
+                return this.hasPermission("Zone2.modifyServerZone");
+            }
+        } else if (inPlayerZone){
+            PlayerZone highestPriorityPlayerZone = PlayerZone.getHighestPlayerZonePriority(playerZoneList);
+            Permission neededPerm = new Permission(this, t);
+            Permission administratePerm = new Permission(this, PermissionType.MANAGE);
+            return highestPriorityPlayerZone.getPerms().contains(neededPerm)
+                    || highestPriorityPlayerZone.getPerms().contains(administratePerm)
+                    || highestPriorityPlayerZone.getPlayerUuid().equals(this.getUniqueId())
+                    || this.hasPermission("Zone2.modifyPlayerZone");
+        } else {
+            return this.hasPermission("Zone2.modifyServerZone");
+        }
+    }
+}
