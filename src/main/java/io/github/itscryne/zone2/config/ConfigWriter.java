8edc0fbb76b50f17f22ba5cb8d64c88a2513c287
Patrick Longer <p.longer@gmx.de>
2020-03-29 20:49:54 +0200

    SubZones

--- src/main/java/io/github/itscryne/zone2/config/ConfigWriter.java
+++ src/main/java/io/github/itscryne/zone2/config/ConfigWriter.java
@@ -7,42 +7,46 @@ import com.google.gson.stream.JsonReader;
 import io.github.itscryne.zone2.Zone2;
 import io.github.itscryne.zone2.spaces.PlayerZone;
 import io.github.itscryne.zone2.spaces.ServerZone;
+import io.github.itscryne.zone2.spaces.SubZone;
 
 import java.io.*;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ConfigWriter {
+public final class ConfigWriter {
     private static ConfigWriter instance;
 
     private final String playerZonePath;
     private final String serverZonePath;
+    private final String subZonePath;
     private Zone2 plugin;
     private File dataDir;
     private File playerZonesFile;
     private File serverZonesFile;
+    private File subZonesFile;
 
     /**
      * ConfigWriter constructor - access via {@link #getInstance(Zone2)} method.
-     * @param plugin the plugin
+     * 
      * @throws IOException if it cant access the files et al
      */
-    private ConfigWriter(Zone2 plugin) throws IOException {
+    private ConfigWriter() throws IOException {
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.setPrettyPrinting().create();
 
-        this.plugin = plugin;
+        this.plugin = Zone2.getPlugin();
         this.dataDir = this.plugin.getDataFolder();
         dataDir.mkdir();
 
         String dataDirPath = dataDir.getAbsolutePath();
         this.serverZonePath = dataDirPath.concat(File.separator).concat("serverZones.json");
         this.playerZonePath = dataDirPath.concat(File.separator).concat("playerZones.json");
+        this.subZonePath = dataDirPath.concat(File.separator).concat("subZones.json");
 
         this.playerZonesFile = new File(playerZonePath);
         playerZonesFile.createNewFile();
-        if (playerZonesFile.length() == 0) { //writing empty List<PlayerZone> to JSON file so we can access it later
+        if (playerZonesFile.length() == 0) { // writing empty List<PlayerZone> to JSON file so we can access it later
             List<PlayerZone> playerZoneList = new ArrayList<>();
 
             Writer fw = new FileWriter(playerZonePath);
@@ -53,7 +57,7 @@ public class ConfigWriter {
 
         this.serverZonesFile = new File(serverZonePath);
         serverZonesFile.createNewFile();
-        if (serverZonesFile.length() == 0) { //writing empty List<ServerZone> to JSON file so we can access it later
+        if (serverZonesFile.length() == 0) { // writing empty List<ServerZone> to JSON file so we can access it later
             List<ServerZone> serverZoneList = new ArrayList<>();
 
             Writer fw = new FileWriter(serverZonePath);
@@ -61,23 +65,35 @@ public class ConfigWriter {
             fw.flush();
             fw.close();
         }
+
+        this.subZonesFile = new File(subZonePath);
+        subZonesFile.createNewFile();
+        if (subZonesFile.length() == 0) { // writing empty List<subZone> to JSON file so we can access it later
+            List<SubZone> subZoneList = new ArrayList<>();
+
+            Writer fw = new FileWriter(subZonePath);
+            gson.toJson(subZoneList, fw);
+            fw.flush();
+            fw.close();
+        }
     }
 
     /**
      * Gets a instance of {@link ConfigReader}
-     * @param plugin the plugin
+     * 
      * @return a ConfigReader instance
      * @throws IOException if it cant access the files et al
      */
-    public static ConfigWriter getInstance(Zone2 plugin) throws IOException {
+    public static synchronized ConfigWriter getInstance() throws IOException {
         if (instance == null) {
-            instance = new ConfigWriter(plugin);
+            instance = new ConfigWriter();
         }
         return instance;
     }
 
     /**
      * Serializes a PlayerZone
+     * 
      * @param zone the PlayerZone to write
      * @throws IOException if it cant access the file et al
      */
@@ -103,6 +119,7 @@ public class ConfigWriter {
 
     /**
      * Serializes a ServerZone
+     * 
      * @param zone the ServerZone to write
      * @throws IOException if it cant access the file et al
      */
@@ -123,10 +140,28 @@ public class ConfigWriter {
         fw.close();
     }
 
+    public void writeSubZone(SubZone zone) throws IOException {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.setPrettyPrinting().create();
+
+        JsonReader subZoneListReader = new JsonReader(new FileReader(this.subZonesFile));
+
+        Type subZoneListType = new TypeToken<List<SubZone>>() {
+        }.getType();
+        List<SubZone> subZoneList = gson.fromJson(subZoneListReader, subZoneListType);
+        subZoneList.add(zone);
+
+        Writer fw = new FileWriter(subZonePath);
+        gson.toJson(subZoneList, fw);
+        fw.flush();
+        fw.close();
+    }
+
     /**
      * Helper function
+     * 
      * @param playerZoneList List to search in
-     * @param id The id to search for
+     * @param id             The id to search for
      * @return the index of the id
      */
     private int findPlayerZoneIndexById(List<PlayerZone> playerZoneList, int id) {
@@ -140,6 +175,7 @@ public class ConfigWriter {
 
     /**
      * Deletes a serialized PlayerZone
+     * 
      * @param id ID of the PlayerZone to delete
      * @throws IOException if it cant access the file et al
      */
@@ -168,8 +204,9 @@ public class ConfigWriter {
 
     /**
      * Helper function
+     * 
      * @param serverZoneList List to search in
-     * @param id The id to search for
+     * @param id             The id to search for
      * @return the index of the id
      */
     private int findServerZoneIndexById(List<ServerZone> serverZoneList, int id) {
@@ -183,6 +220,7 @@ public class ConfigWriter {
 
     /**
      * Deletes a serialized ServerZone
+     * 
      * @param id ID of the ServerZone to delete
      * @throws IOException if it cant access the file et al
      */
@@ -208,6 +246,45 @@ public class ConfigWriter {
         fw.flush();
         fw.close();
     }
+
+    private int findSubZoneIndexById(List<SubZone> subZoneList, int id) {
+        for (SubZone p : subZoneList) {
+            if (p.getId() == id) {
+                return subZoneList.indexOf(p);
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Deletes a serialized subZone
+     * 
+     * @param id ID of the subZone to delete
+     * @throws IOException if it cant access the file et al
+     */
+    public void deleteSubZone(int id) throws IOException {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.setPrettyPrinting().create();
+
+        JsonReader subZoneListReader = new JsonReader(new FileReader(this.subZonesFile));
+
+        Type subZoneListType = new TypeToken<List<SubZone>>() {
+        }.getType();
+        List<SubZone> subZoneList = gson.fromJson(subZoneListReader, subZoneListType);
+
+        int indexToDelete = findSubZoneIndexById(subZoneList, id);
+        if (indexToDelete == -1) {
+            return;
+        } else {
+            subZoneList.remove(indexToDelete);
+        }
+
+        Writer fw = new FileWriter(subZonePath);
+        gson.toJson(subZoneList, fw);
+        fw.flush();
+        fw.close();
+    }
+
     /**
      * Destroys the ConfigWriter instance
      */
