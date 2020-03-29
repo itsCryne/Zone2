973ec31d9e83d496af6e287d3146750e10685921
Patrick Longer <p.longer@gmx.de>
2020-03-29 20:50:15 +0200

    Refactoring, Item Frames

--- src/main/java/io/github/itscryne/zone2/events/Zone2BuildTypeEvent.java
+++ src/main/java/io/github/itscryne/zone2/events/Zone2BuildTypeEvent.java
@@ -5,59 +5,88 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.player.PlayerBucketFillEvent;
+import org.bukkit.event.player.PlayerBucketEmptyEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
-import io.github.itscryne.zone2.Zone2;
 import io.github.itscryne.zone2.perms.PermissionType;
 import io.github.itscryne.zone2.extensions.Zonecation;
 import io.github.itscryne.zone2.extensions.Zoneler;
 
 public class Zone2BuildTypeEvent implements Listener {
-    public Zone2BuildTypeEvent(){}
 
     @EventHandler
-    public void onBlockPlace (BlockPlaceEvent event) throws IOException {
+    public void onBlockPlace(BlockPlaceEvent event) throws IOException {
         Zonecation eventLocation = new Zonecation(event.getBlock().getLocation());
         Zoneler eventPlayer = new Zoneler(event.getPlayer());
 
         boolean allowed = eventPlayer.isAllowed(eventLocation, PermissionType.BUILD);
-        Zone2.getPlugin().getLogger().info(String.valueOf(allowed));
+        event.setCancelled(!allowed);
+        if (!allowed) {
+            eventPlayer.sendXPMessage(ChatColor.RED + "Das darfst du hier nicht", true);
+        }
     }
 
     @EventHandler
-    public void onPlayerBucketFill (PlayerBucketFillEvent event) throws IOException {
+    public void onPlayerBucketEmpty(PlayerBucketEmptyEvent event) throws IOException {
         Zonecation eventLocation = new Zonecation(event.getBlock().getLocation());
         Zoneler eventPlayer = new Zoneler(event.getPlayer());
 
         boolean allowed = eventPlayer.isAllowed(eventLocation, PermissionType.BUILD);
-        Zone2.getPlugin().getLogger().info(String.valueOf(allowed));
+        event.setCancelled(!allowed);
+        if (!allowed) {
+            eventPlayer.sendXPMessage(ChatColor.RED + "Das darfst du hier nicht", true);
+        }
     }
 
     @EventHandler
-    public void onPlayerInteract (PlayerInteractEvent event) throws IOException { //TODO: Just Farmland, sth else?
-        List<Material> hoes = new ArrayList<>(Arrays.asList(
-            Material.WOODEN_HOE,
-            Material.STONE_HOE,
-            Material.GOLDEN_HOE,
-            Material.IRON_HOE,
-            Material.DIAMOND_HOE
-        ));
+    public void onPlayerInteract(PlayerInteractEvent event) throws IOException {
+        List<Material> hoes = new ArrayList<>(Arrays.asList(Material.WOODEN_HOE, Material.STONE_HOE,
+                Material.GOLDEN_HOE, Material.IRON_HOE, Material.DIAMOND_HOE));
 
-        if(event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
-        if(event.getClickedBlock() == null) return;
-        if(event.getItem() == null) return;
-        if(!hoes.contains(event.getItem().getType())) return;
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK){
+            return;
+        }
+            if (event.getClickedBlock() == null){
+            return;
+        }
+            if (event.getItem() == null){
+            return;
+        }
+            if (hoes.contains(event.getItem().getType())){
+            return;
+        }
 
         Zonecation eventLocation = new Zonecation(event.getClickedBlock().getLocation());
         Zoneler eventPlayer = new Zoneler(event.getPlayer());
 
         boolean allowed = eventPlayer.isAllowed(eventLocation, PermissionType.BUILD);
-        Zone2.getPlugin().getLogger().info(String.valueOf(allowed));
+        event.setCancelled(!allowed);
+        if (!allowed) {
+            eventPlayer.sendXPMessage(ChatColor.RED + "Das darfst du hier nicht", true);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerInteractEntity(PlayerInteractEntityEvent event) throws IOException {
+        if (event.getRightClicked().getType() != EntityType.ITEM_FRAME){
+            return;
+        }
+
+        Zonecation eventLocation = new Zonecation(event.getRightClicked().getLocation());
+        Zoneler eventPlayer = new Zoneler(event.getPlayer());
+
+        boolean allowed = eventPlayer.isAllowed(eventLocation, PermissionType.BUILD);
+        event.setCancelled(!allowed);
+        if (!allowed) {
+            eventPlayer.sendXPMessage(ChatColor.RED + "Das darfst du hier nicht", true);
+        }
     }
 }
