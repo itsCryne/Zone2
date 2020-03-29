4a89f06f4b3e3a3bd17bfbd71ebb9c9012379a67
Patrick Longer <p.longer@gmx.de>
2020-03-29 20:54:10 +0200

    Refactoring

--- src/main/java/io/github/itscryne/zone2/perms/PermissionType.java
+++ src/main/java/io/github/itscryne/zone2/perms/PermissionType.java
@@ -1,10 +1,8 @@
 package io.github.itscryne.zone2.perms;
 
-import java.io.Serializable;
-
 /**
  * @serial JSON
  */
-public enum PermissionType implements Serializable {
+public enum PermissionType {
     DESTROY, BUILD, REDSTONE, DOORS, INVENTORY, ENTITY, TRANSPORT, MANAGE
 }
