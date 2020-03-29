ca35e1937cd39bc528fee7d37f2f76f8da284b7c
Patrick Longer <p.longer@gmx.de>
2020-03-29 20:55:19 +0200

    OOP refactoring

--- src/main/java/io/github/itscryne/zone2/spaces/ServerZone.java
+++ src/main/java/io/github/itscryne/zone2/spaces/ServerZone.java
@@ -1,37 +1,33 @@
 package io.github.itscryne.zone2.spaces;
 
-import io.github.itscryne.zone2.config.ConfigReader;
-import io.github.itscryne.zone2.Zone2;
-import org.bukkit.World;
-
-import java.io.IOException;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
+import org.bukkit.World;
+
 import io.github.itscryne.zone2.extensions.Zonecation;
+import io.github.itscryne.zone2.perms.Permission;
+
 /**
  * @serial JSON
  */
-public class ServerZone extends Zone implements Serializable {
-    private String name;
+public class ServerZone extends Zone {
 
     /**
      * Constructor for Zonecations
+     * 
      * @param l1       First Zonecation (higher coordinates)
      * @param l2       Second Zonecation (lower coordinates)
      * @param priority Priority of the Zone
      * @param id       ID of the Zone
      * @param name     Name of the Zone
      */
-    public ServerZone(Zonecation l1, Zonecation l2, int priority, int id, String name) {
-        super(l1, l2, priority, id);
-        this.name = name;
+    public ServerZone(Zonecation l1, Zonecation l2, int priority, int id, String name, List<Permission> perms) {
+        super(l1, l2, priority, id, name, perms);
     }
 
     /**
      * Constructor for raw coordinates
+     * 
      * @param hx       Hgher x coordinate
      * @param lx       Lower x coordinate
      * @param hy       Higher y coordinate
@@ -43,81 +39,7 @@ public class ServerZone extends Zone implements Serializable {
      * @param id       ID of the Zone
      * @param name     Name of the Zone
      */
-    //TODO: Move name to Zone
-    public ServerZone(int hx, int lx, int hy, int ly, int hz, int lz, World w, int priority, int id, String name) {
-        super(hx, lx, hy, ly, hz, lz, w, priority, id);
-        this.name = name;
-    }
-
-    /**
-     * Checks wether the instance Zone collides with any other Zone
-     * @param plugin the plugin
-     * @return wether the instance collides
-     * @throws IOException if it cant find a file et al
-     */
-    public boolean collidesWithAnyZone(Zone2 plugin) throws IOException {
-        ConfigReader reader = ConfigReader.getInstance(plugin);
-        List<PlayerZone> playerZoneList = reader.getPlayerZoneList();
-        List<ServerZone> serverZoneList = reader.getServerZoneList();
-
-        if (playerZoneList != null) {
-            if (!playerZoneList.isEmpty()) {
-                for (PlayerZone i : playerZoneList) {
-                    i.setL1(new Zonecation(Zonecation.deserialize(i.getSerL1())));
-                    i.setL2(new Zonecation(Zonecation.deserialize(i.getSerL2())));
-
-                    if ((this.getL2().getX() <= i.getL1().getX() && this.getL1().getX() >= i.getL2().getX())
-                            && (this.getL2().getY() <= i.getL1().getY() && this.getL1().getY() >= i.getL2().getY())
-                            && (this.getL2().getZ() <= i.getL1().getZ() && this.getL1().getZ() >= i.getL2().getZ())
-                            && (this.getL1().getWorld().equals(i.getL1().getWorld()))) { //TODO does this really work like intended?
-                        return true;
-                    }
-                }
-            }
-        }
-
-        if (playerZoneList != null) {
-            if (!playerZoneList.isEmpty()) {
-                for (ServerZone i : serverZoneList) {
-                    i.setL1(new Zonecation(Zonecation.deserialize(i.getSerL1())));
-                    i.setL2(new Zonecation(Zonecation.deserialize(i.getSerL2())));
-
-                    if ((this.getL2().getX() <= i.getL1().getX() && this.getL1().getX() >= i.getL2().getX())
-                            && (this.getL2().getY() <= i.getL1().getY() && this.getL1().getY() >= i.getL2().getY())
-                            && (this.getL2().getZ() <= i.getL1().getZ() && this.getL1().getZ() >= i.getL2().getZ())
-                            && (this.getL1().getWorld().equals(i.getL1().getWorld()))) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Returns the next available ID
-     * @param plugin the plugin
-     * @return the ID
-     * @throws IOException if it cant find a file et al
-     */
-    public static int getNextId(Zone2 plugin) throws IOException {//TODO: -> Zone
-        ConfigReader reader = ConfigReader.getInstance(plugin);
-        List<ServerZone> serverZoneList = reader.getServerZoneList();
-
-        if (serverZoneList == null || serverZoneList.isEmpty()){
-            return 0;
-        }
-
-        List<Integer> zoneIds = new ArrayList<>();
-        serverZoneList.forEach(element -> zoneIds.add(element.getId()));
-
-        return Collections.max(zoneIds) + 1;
-    }
-
-    /**
-     * @return Name of the zone
-     */
-    public String getName() {
-        return this.name;
+    public ServerZone(int hx, int lx, int hy, int ly, int hz, int lz, World w, int priority, int id, String name, List<Permission> perms) {
+        super(hx, lx, hy, ly, hz, lz, w, priority, id, name, perms);
     }
 }
