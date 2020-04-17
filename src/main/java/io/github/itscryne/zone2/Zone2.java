a2009fc9cd4d9cd8ef8ea5b6094b837d1bb5ed61
Patrick Longer <p.longer@gmx.de>
2020-04-17 21:02:46 +0200

    Dynmap Integration

--- src/main/java/io/github/itscryne/zone2/Zone2.java
+++ src/main/java/io/github/itscryne/zone2/Zone2.java
@@ -2,11 +2,13 @@ package io.github.itscryne.zone2;
 
 import io.github.itscryne.zone2.commands.Zone2CreateServerZoneCommand;
 import io.github.itscryne.zone2.commands.Zone2CreateSubZoneCommand;
+import io.github.itscryne.zone2.commands.Zone2DeleteSubZoneCommand;
 import io.github.itscryne.zone2.commands.Zone2InfoCommand;
 import io.github.itscryne.zone2.commands.Zone2CreatePlayerZoneCommand;
 import io.github.itscryne.zone2.commands.Zone2MyZonesCommand;
 import io.github.itscryne.zone2.commands.Zone2PermissionCommand;
 import io.github.itscryne.zone2.commands.Zone2ServerPermissionCommand;
+import io.github.itscryne.zone2.config.ConfigReader;
 import io.github.itscryne.zone2.events.Zone2BuildTypeEvent;
 import io.github.itscryne.zone2.events.Zone2DestroyTypeEvent;
 import io.github.itscryne.zone2.events.Zone2DoorsTypeEvent;
@@ -15,14 +17,21 @@ import io.github.itscryne.zone2.events.Zone2EventListener;
 import io.github.itscryne.zone2.events.Zone2InventoryTypeEvent;
 import io.github.itscryne.zone2.events.Zone2RedstoneTypeEvent;
 import io.github.itscryne.zone2.events.Zone2TransportTypeEvent;
+import io.github.itscryne.zone2.spaces.PlayerZone;
+import io.github.itscryne.zone2.spaces.ServerZone;
 
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.JavaPluginLoader;
+import org.dynmap.DynmapAPI;
+import org.dynmap.markers.MarkerSet;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.logging.Level;
 
 /**
  * @author Patrick Longer
@@ -30,6 +39,8 @@ import java.io.Serializable;
  */
 public final class Zone2 extends JavaPlugin implements Serializable {
     private static Zone2 plugin;
+    private static MarkerSet markers;
+    private static MarkerSet serverMarkers;
 
     public Zone2() {
         super();
@@ -43,6 +54,14 @@ public final class Zone2 extends JavaPlugin implements Serializable {
         return plugin;
     }
 
+    public static MarkerSet getMarkers() {
+        return markers;
+    }
+
+    public static MarkerSet getServerMarkers() {
+        return serverMarkers;
+    }
+
     /**
      * Called when this plugin is enabled
      */
@@ -53,6 +72,7 @@ public final class Zone2 extends JavaPlugin implements Serializable {
         // TODO: Player can have multiple Zones?
         plugin = this;
         plugin.saveDefaultConfig();
+
         // Plugin startup logic
         this.getCommand("createzone").setExecutor(new Zone2CreatePlayerZoneCommand());
         this.getCommand("createserverzone").setExecutor(new Zone2CreateServerZoneCommand());
@@ -61,6 +81,7 @@ public final class Zone2 extends JavaPlugin implements Serializable {
         this.getCommand("createsubzone").setExecutor(new Zone2CreateSubZoneCommand());
         this.getCommand("zoneinfo").setExecutor(new Zone2InfoCommand());
         this.getCommand("serverpermission").setExecutor(new Zone2ServerPermissionCommand());
+        this.getCommand("deletesubzone").setExecutor(new Zone2DeleteSubZoneCommand());
 
         Bukkit.getPluginManager().registerEvents(new Zone2EventListener(), this);
         Bukkit.getPluginManager().registerEvents(new Zone2DestroyTypeEvent(), this);
@@ -71,6 +92,34 @@ public final class Zone2 extends JavaPlugin implements Serializable {
         Bukkit.getPluginManager().registerEvents(new Zone2RedstoneTypeEvent(), this);
         Bukkit.getPluginManager().registerEvents(new Zone2TransportTypeEvent(), this);
 
+        if (getServer().getPluginManager().getPlugin("dynmap") == null) {
+            this.getLogger().warning("Dynmap not found. Not enabling compat");
+            return;
+        }
+        this.getLogger().warning("Dynmap found, Zones will be displayed on it");
+        Plugin dynmap = getServer().getPluginManager().getPlugin("dynmap");
+        DynmapAPI api = (DynmapAPI) dynmap;
+
+        markers = api.getMarkerAPI().createMarkerSet("zonen", "Zonen", null, false);
+        serverMarkers = api.getMarkerAPI().createMarkerSet("serverzonen", "Serverzonen", null, false);
+
+        ConfigReader reader = null;
+        try {
+            reader = ConfigReader.getInstance();
+            for (PlayerZone zone : reader.getPlayerZoneList()) {
+                zone.displayAreaMarker(markers);
+            }
+
+            for (ServerZone zone : reader.getServerZoneList()) {
+                zone.displayAreaMarker(serverMarkers);
+            }
+        } catch (IOException e) {
+            this.getLogger().log(Level.SEVERE, e.getMessage(), e.getCause());
+        } finally {
+            if (reader != null) {
+                ConfigReader.destroy();
+            }
+        }
     }
 
     /**
