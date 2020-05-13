e3e67a5c26c0f7df91078d52205d087b7eb86abc
Patrick Longer <p.longer@gmx.de>
2020-05-13 16:44:52 +0200

    Remove unnecessary ZLocation constructor Remove duplicate check whether subZoneList is empty Remove getSubZones()

--- src/main/java/io/github/itscryne/zone2/spaces/SubZone.java
+++ src/main/java/io/github/itscryne/zone2/spaces/SubZone.java
@@ -7,7 +7,6 @@ import org.bukkit.Location;
 import org.bukkit.World;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -27,13 +26,6 @@ public class SubZone extends Zone {
 
     // TODO: Are Subzones always preferred over normal Zones?
 
-    public SubZone(ZLocation l1, ZLocation l2, int priority, int id, String name, UUID playerUuid,
-                   List<Permission> perms, UUID superZoneUUID) {
-        super(l1, l2, priority, id, name, perms);
-        this.playerUUID = playerUuid;
-        this.superZoneUUID = superZoneUUID;
-    }
-
     public boolean collidesWithAnySubZone() throws IOException {
         ConfigReader reader = ConfigReader.getInstance();
         List<SubZone> subZoneList = reader.getSubZoneList();
@@ -42,31 +34,23 @@ public class SubZone extends Zone {
             return false;
         }
 
-        if (!subZoneList.isEmpty()) {
-            for (SubZone i : subZoneList) {
-                i.setL1(new ZLocation(Location.deserialize(i.getSerL1())));
-                i.setL2(new ZLocation(Location.deserialize(i.getSerL2())));
+        for (SubZone i : subZoneList) {
+            i.setL1(new ZLocation(Location.deserialize(i.getSerL1())));
+            i.setL2(new ZLocation(Location.deserialize(i.getSerL2())));
 
-                if (this.getL2().getX() <= i.getL1().getX() && this.getL1().getX() >= i.getL2().getX()
-                        && this.getL2().getY() <= i.getL1().getY() && this.getL1().getY() >= i.getL2().getY()
-                        && this.getL2().getZ() <= i.getL1().getZ() && this.getL1().getZ() >= i.getL2().getZ()
-                        && this.getL1().getWorld().equals(i.getL1().getWorld())) {
-                    return true;
-                }
+            if (this.getL2().getX() <= i.getL1().getX() && this.getL1().getX() >= i.getL2().getX()
+                    && this.getL2().getY() <= i.getL1().getY() && this.getL1().getY() >= i.getL2().getY()
+                    && this.getL2().getZ() <= i.getL1().getZ() && this.getL1().getZ() >= i.getL2().getZ()
+                    && this.getL1().getWorld().equals(i.getL1().getWorld())) {
+                return true;
             }
         }
         return false;
     }
 
-    @Override
-    public List<SubZone> getSubZones() {
-        return new ArrayList<SubZone>();
-    }
-
     public UUID getPlayerUUID() {
         return this.playerUUID;
     }
-
     public UUID getSuperZoneUUID() {
         return this.superZoneUUID;
     }
